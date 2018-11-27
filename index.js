026b87e81c416d6e1878164021e022761af5dd2b
Sergey Chikuyonok <serge.che@gmail.com>
2018-11-27 13:16:05 +0300

    Updated dependencies

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 var gulpPlugin = require('./lib/gulp');
-var notifier = require('node-notifier');
 
 module.exports = function(obj) {
 	if (typeof obj === 'object' && obj.registerMultiTask) {
-		throw new Error('Grunt is not currently supported');;
+		throw new Error('Grunt is not currently supported');
 	}
 
 	return module.exports.gulp(obj).on('error', function(err) {
-		notifier.notify({
-			title: 'Error', 
-			message: err,
-			sound: true
-		});
 		console.error(err.stack || err);
 		this.emit('end');
 	});
